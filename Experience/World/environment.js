76563a5311036c2f0fe73f09892d688c9df737b7
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-04-21 16:24:53 +0700

    css change

--- Experience/World/environment.js
+++ Experience/World/environment.js
@@ -32,7 +32,7 @@ export default class Environment {
     }
 
     setSunlight() {
-        this.sunLight = new THREE.DirectionalLight("#ffffff", 3);
+        this.sunLight = new THREE.DirectionalLight("#ffffff", 1);
         this.sunLight.castShadow = true;
         this.sunLight.shadow.camera.far = 20;
         this.sunLight.shadow.mapSize.set(2048, 2048);
